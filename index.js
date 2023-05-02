de7309945a7c1447cbdf18dc4ec382aecc15a833
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2023-05-02 14:45:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
 
-exec('/start.sh', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`执行脚本时出错: ${error}`);
-    return;
-  }
-  console.log(`输出: ${stdout}`);
-  console.error(`错误: ${stderr}`);
+const startScript = spawn('/start.sh');
+
+startScript.stdout.on('data', (data) => {
+  console.log(`输出：${data}`);
+});
+
+startScript.stderr.on('data', (data) => {
+  console.error(`错误：${data}`);
+});
+
+startScript.on('close', (code) => {
+  console.log(`子进程退出，退出码 ${code}`);
 });
