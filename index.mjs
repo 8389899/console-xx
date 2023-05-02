4878c1a3fbfda710f6aac63cd48be945d16e68b4
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2023-05-02 14:04:13 +0800

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,9 +1,10 @@
 const { exec } = require('child_process');
 
-exec('sh /start.sh', (error, stdout, stderr) => {
+exec('sh backup.sh', (error, stdout, stderr) => {
   if (error) {
-    console.error(`执行Shell脚本时出错：${error}`);
+    console.error(`执行脚本时出错: ${error}`);
     return;
   }
-  console.log(`脚本的输出：${stdout}`);
+  console.log(`输出: ${stdout}`);
+  console.error(`错误: ${stderr}`);
 });
