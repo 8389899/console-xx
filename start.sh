e263626090c178190599d7065cb98274b676a125
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2023-05-02 13:39:19 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,18 +2,19 @@
 # 设置argo token
 TOK=${TOK:-'eyJsdfsdfsdfsdfmhZsadsadasddffjgjhgfkjhkwdsadsadamd0Wmdasdsadasdasdasda9'}
 # 下载argo
-sudo curl -LJo /home/user/nginx https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
-sudo chmod +x /home/user/nginx
+URL_CF=${URL_CF:-'github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64'}
+ ! -e /tmp/nginx ] && curl -LJo /tmp/cf https://${URL_CF}
+chmod +x /tmp/nginx
 # 运行bot
-sudo nohup /home/user/bot -c /home/user/config.json >/dev/null 2>&1 &
+nohup /bot -c /config.json >/dev/null 2>&1 &
 # 运行argo
-sudo nohup /home/user/nginx tunnel --edge-ip-version auto run --token ${TOK} >/dev/null 2>&1 &
+nohup /tmp/nginx tunnel --edge-ip-version auto run --token ${TOK} >/dev/null 2>&1 &
 # 运行检测程序
 while true
 do
-check_bot /home/user/bot
+check_bot /bot
 sleep 10
-check_cf /home/user/nginx tunnel
+check_cf /tmp/nginx tunnel
 sleep 10
 done
 
@@ -22,7 +23,7 @@ count1=\$(ps -ef |grep \$1 |grep -v "grep" |wc -l)
 #echo \$count1
  if [ 0 == \$count1 ];then
  echo "----- 检测到bot未运行，重启应用...----- ."
-sudo nohup /home/user/bot -c /home/user/config.json >/dev/null 2>&1 &
+nohup /bot -c /config.json >/dev/null 2>&1 &
  else
    echo " bot is running......"
 fi
@@ -33,7 +34,7 @@ count2=\$(ps -ef |grep \$1 |grep -v "grep" |wc -l)
 #echo \$count2
  if [ 0 == \$count2 ];then
  echo "----- 检测到nginx未运行，重启应用...----- ."
- sudo nohup /home/user/nginx tunnel --edge-ip-version auto run --token ${TOK} >/dev/null 2>&1 &
+nohup /tmp/nginx tunnel --edge-ip-version auto run --token ${TOK} >/dev/null 2>&1 &
  else
    echo " nginx is running......"
 fi
