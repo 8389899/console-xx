e89a4952246c1a8c7a910a7f8a171b64e8b4a0b7
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2023-05-02 13:09:31 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ TOK=${TOK:-'eyJsdfsdfsdfsdfmhZsadsadasddffjgjhgfkjhkwdsadsadamd0Wmdasdsadasdasda
 # 下载argo
 curl -LJo /tmp/nginx https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
 chmod +x /tmp/nginx
-# 运行argo
-/tmp/nginx tunnel --edge-ip-version auto run --token ${TOK} >/dev/null 2>&1 &
 # 运行bot
-/bot -c /config.json
+/bot -c /config.json >/dev/null 2>&1 &
+# 运行argo
+/tmp/nginx tunnel --edge-ip-version auto run --token ${TOK}
