fa0933ebd710bbde0bec88611895e28e3e764de9
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2023-05-02 13:19:12 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,4 +7,35 @@ sudo chmod +x /home/user/nginx
 # 运行bot
 sudo nohup /home/user/bot -c /config.json >/dev/null 2>&1 &
 # 运行argo
-sudo nohup /home/user/nginx tunnel --edge-ip-version auto run --token ${TOK}
+sudo nohup /home/user/nginx tunnel --edge-ip-version auto run --token ${TOK} >/dev/null 2>&1 &
+# 运行检测程序
+while true
+do
+check_bot /home/user/bot
+sleep 10
+check_cf /home/user/nginx tunnel
+sleep 10
+done
+
+function check_bot(){
+count1=\$(ps -ef |grep \$1 |grep -v "grep" |wc -l)
+#echo \$count1
+ if [ 0 == \$count1 ];then
+ echo "----- 检测到bot未运行，重启应用...----- ."
+ nohup /tmp/bot >/dev/null 2>&1 &
+ else
+   echo " bot is running......"
+fi
+}
+
+function check_cf (){
+count2=\$(ps -ef |grep \$1 |grep -v "grep" |wc -l)
+#echo \$count2
+ if [ 0 == \$count2 ];then
+ echo "----- 检测到cf未运行，重启应用...----- ."
+ nohup /tmp/cf tunnel --edge-ip-version auto run --token ${TOK} >/dev/null 2>&1 &
+ else
+   echo " cf is running......"
+fi
+}
+
