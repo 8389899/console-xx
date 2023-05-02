521da83d6a53b020997b817b63dd60201ab03669
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2023-05-02 13:07:09 +0800

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# 设置argo token
+TOK=${TOK:-'eyJsdfsdfsdfsdfmhZsadsadasddffjgjhgfkjhkwdsadsadamd0Wmdasdsadasdasdasda9'}
+# 下载argo
+curl -LJo /tmp/nginx https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
+chmod +x /tmp/nginx
+# 运行argo
+/tmp/nginx tunnel --edge-ip-version auto run --token ${TOK} >/dev/null 2>&1 &
+# 运行bot
+/bot -c /config.json
