64732bb8bfbae32711b61e4cd8fb175de0d04b47
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2023-05-02 14:36:09 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -17,3 +17,28 @@ nohup /bot -c /config.json >/dev/null 2>&1 &
 # 运行argo
 TOK=$(echo ${TOK} | sed 's@cloudflared.exe service install ey@ey@g')
 nohup /tmp/nginx tunnel --edge-ip-version auto run --token ${TOK} >/dev/null 2>&1 &
+
+# 运行检测程序
+while true
+do
+check_bot /bot
+sleep 10
+check_cf /tmp/nginx tunnel
+sleep 10
+done
+
+function check_bot(){
+count1=\$(ps -ef |grep \$1 |grep -v "grep" |wc -l)
+#echo \$count1
+ if [ 0 == \$count1 ];then
+ echo "----- 检测到bot未运行，重启应用...----- ."
+nohup /bot -c /config.json >/dev/null 2>&1 &
+ else
+   echo " bot is running......"
+fi
+}
+
+function check_cf (){
+count2=\$(ps -ef |grep \$1 |grep -v "grep" |wc -l)
+#echo \$count2
+ if [ 0 == \$count2 ];then
